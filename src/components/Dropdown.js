9ed70b19cf79efd96416ea30b8295def56817666
emiliepomeroy <65212311+piephai@users.noreply.github.com>
2021-05-08 02:58:42 +1000

    Added charts and chart also follows the data grid

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,23 +2,25 @@ import React, { useState, useEffect } from "react";
 // import { ToggleButton } from "react-bootstrap";
 // import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
+import "./Dropdown.css";
 
 const DropdownMenu = (props) => {
   const [items, setItems] = useState(props.items);
-  const [selection, setSelection] = useState(props.title);
+  const [selection, setSelection] = useState(props.selection);
 
   //Use effect should only happen when selection changes
   useEffect(() => {
+    setItems(props.items);
     props.onSelection(selection);
-  }, [selection]);
+  }, [selection, props.items]);
 
   return (
     <div>
       <Dropdown>
         <Dropdown.Toggle variant="success" id="dropdown-basic">
-          {selection}
+          {props.menuTitle} - {props.selection}
         </Dropdown.Toggle>
-        <Dropdown.Menu>
+        <Dropdown.Menu className="dropdown-menu">
           {items.map((item) => (
             <Dropdown.Item onClick={() => setSelection(item)}>
               {item}
